6520377fd9070372bfd3ff7aacd6c0bed5f2ca94
Andrew <andrewmll94@gmail.com>
2022-10-16 16:50:01 -0300

    Conclusão do requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,10 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se é uma função.', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('Verifica se o retorno é o esperado.', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
 });
